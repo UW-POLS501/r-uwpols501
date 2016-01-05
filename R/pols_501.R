0041982233ec88688d2b55d66f8f0c64b1610520
jrnold <jeffrey.arnold@gmail.com>
2016-01-05 12:33:38 -0800

    clean code, add project to create_assignment_skeleton

--- R/pols_501.R
+++ R/pols_501.R
@@ -1,22 +1,23 @@
-#' @import assertthat
+#" @import assertthat
+#' @importFrom devtools add_rstudio_project
 NULL
 
 #' Create Directory Structure for an Assignment
 #'
 #' @param assignment Assignment number
-#' @param netId Student netID. Example: jarnold
+#' @param net_id Student netID. Example: jarnold
 #' @param dir Directory in which the assignment will be created
 #' @export
-create_assignment_skeleton <- function(assignment, netId, dir='.') {
+create_assignment_skeleton <- function(assignment, net_id, dir = ".") {
   assert_that(is.numeric(assignment))
-  mainDir <- file.path(dir, paste0('assignment', assignment, '-', netId))
-  dir.create(mainDir)
-  dirNames <- c('data','data-raw','figures','output')
-  for (dir in dirNames){
-    dir.create(file.path(mainDir,dir))
+  main_dir <- file.path(dir, paste0("assignment", assignment, "-", net_id))
+  dir.create(main_dir)
+  dir_names <- c("data", "data-raw","figures","output")
+  add_rstudio_project(main_dir)
+  for (dir in dir_names) {
+    dir.create(file.path(main_dir,dir))
   }
-  cat(paste0('Code and data for POLS-501 assignment ', assignment, '\n'),
-             file= file.path(mainDir,'README.md'))
-  cat(paste('Assignment', assignment, 'in', mainDir))
+  cat(paste0("Code and data for POLS-501 assignment ", assignment, "\n"),
+             file= file.path(main_dir,"README.md"))
+  cat(paste("Assignment", assignment, "in", main_dir))
 }
-
