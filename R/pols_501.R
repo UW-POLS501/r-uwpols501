47fc2b6247cc669af83ac5632db38490286961c3
jrnold <jeffrey.arnold@gmail.com>
2016-01-29 17:32:13 -0800

    fix *_skeleton functions

--- R/pols_501.R
+++ R/pols_501.R
@@ -10,8 +10,8 @@ globalVariables(c("everything"))
 
 .create_project_dirs <- function(d) {
   dir.create(d)
-  dir_names <- c("data", "data-raw", "figures", "output")
-  #add_rstudio_project(d)
+  dir_names <- c("data", "figure", "output")
+  add_rstudio_project(d)
   for (dn in dir_names) {
     dir.create(file.path(d, dn))
   }
@@ -19,27 +19,37 @@ globalVariables(c("everything"))
 
 #' Create Directory Structure for Projects
 #'
+#' Creates a directory with the correct directory structure.
+#' Use `create_assignment_skeleton` for computation assignments,
+#' and `create_research_project_skeleton` for research projects.
+#'
 #' @param assignment Assignment number
-#' @param net_id Student netID. Example: jarnold
-#' @param dir Directory in which the assignment will be created
+#' @param net_id Student netID. Example:
+#' @param dst Directory in which the project will be created
+#' @param dirname Directory name of the project
 #' @export
-create_assignment_skeleton <- function(assignment, net_id, dir = ".") {
-  assert_that(is.numeric(assignment) | is.character(assignment))
+create_assignment_skeleton <- function(assignment, net_id, dst = ".") {
+  assert_that(is.numeric(assignment))
   main_dir <- file.path(dir, paste0("assignment-", assignment, "-", net_id))
-  .create_project_dirs(main_dir)
-  cat(paste0("Code and data for POLS-501 assignment ", assignment, "\n"),
-             file = file.path(main_dir, "README.md"))
-  cat(paste("Createted empty project for Assignment",
-            assignment, "in", main_dir))
+  create_rproject_skeleton(main_dir)
 }
 
+#' @rdname create_assignment_skeleton
+#' @export
+create_research_project_skeleton <- function(assignment, net_id, dst = ".") {
+  assert_that(is.numeric(assignment))
+  main_dir <- file.path(dir, paste0("project-", assignment, "-", net_id))
+  create_rproject_skeleton(main_dir)
+}
 
 
 #' @rdname create_assignment_skeleton
 #' @export
-create_project_skeleton <- function(dir = ".") {
-  .create_project_dirs(dir)
-  cat(paste("Created empty project in ", dir))
+create_rproject_skeleton <- function(dirname, dst = ".") {
+  .create_project_dirs(dirname)
+  cat(paste("Created empty R project for data analysis in ", dir))
+  print(dir(dirname, recursive = TRUE))
+  invisible(dirname)
 }
 
 #' Zip project or assignment for submission
@@ -186,26 +196,4 @@ plyr::revalue
 
 #' @export
 plyr::mapvalues
-
-#' Plot distributions
-#'
-#'
-plot_pdf <- function(distribution, ..., left, pmin = 0.005, pmax = 0.995,
-         xmin = NULL, xmax = NULL, n = 101) {
-  dfun <- match.fun(sprintf("d%s", distribution))
-  qfun <- match.fun(sprintf("q%s", distribution))
-
-  if (is.null(xmin)) {
-    xmin <- quantile_fun(pmin, ...)
-  }
-  if (is.null(xmax)) {
-    xmax <- quantile_fun(pmax, ...)
-  }
-  .data <-
-    data_frame(x = seq(xmin, xmax, length.out = n),
-               y = dfun(x, ...))
-
-
-
-}
 
