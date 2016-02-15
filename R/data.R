1b2e0319e229f9f670a18f39d5457256ffcff3e1
jrnold <jeffrey.arnold@gmail.com>
2016-02-15 15:30:07 -0800

    add turnout data

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#'
+
+#' Turnout Data Set from the 1992 American National Election Survey
+#'
+#' This data set contains individual-level turnout data. It pools several
+#' American National Election Surveys conducted during the 1992 presidential
+#' election year.  Only the first 2,000 observations (from a total of 15,837
+#'                                                     observations) are included in the sample data.
+#'
+#' @format
+#' A table containing 5 variables ("race", "age", "educate",  "income", and "vote") and 2,000 observations.
+#'
+#' @source Zelig package
+#' @references
+#'  King, Gary, Michael Tomz, Jason Wittenberg (2000).
+#'   ``Making the Most of Statistical Analyses: Improving Interpretation and
+#'   Presentation,'' \emph{American Journal of Political Science}, vol. 44,
+#'   pp.341--355.
+"turnout"
