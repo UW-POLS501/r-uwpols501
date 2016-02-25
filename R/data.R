8f918be70ddede54e0a2a3c1db8817f5e6b7db6f
jrnold <jeffrey.arnold@gmail.com>
2016-02-25 12:11:27 -0800

    Added Iversen and Soskice data

--- R/data.R
+++ R/data.R
@@ -17,3 +17,20 @@
 #'   Presentation,'' \emph{American Journal of Political Science}, vol. 44,
 #'   pp.341--355.
 "turnout"
+
+#' Party Systems and Redistributive Government
+#'
+#' @format A data frame containng 4 variables and 14 observations.
+#'
+#' \describe{
+#'  \item{cty}{Country}
+#'  \item{elec_sys}{Electoral system: Majoritarian "maj", Proportional representation "pr", unamimity government "unam"}
+#'  \item{povred}{Percent of citizens liftedn out of poverty by taxes and transfers.}
+#'  \item{enp}{Effective number of parties.}
+#' }
+#'
+#' @references
+#'  Torben Iversen and David Soskice. 2006. ``Electoral Institutions and the Politics of Coalitions: Why Some
+#'  Democracies Redistribute More Than Others.`` \emph{American Political Science Review}.
+#'
+"iver"
