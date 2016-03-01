92499871425531d92216068f024b4f72052ed1fe
CasAndreu <acasas2@uw.edu>
2016-03-01 11:43:08 -0800

    adding Prestige dataset

--- R/data.R
+++ R/data.R
@@ -34,3 +34,25 @@
 #'  Democracies Redistribute More Than Others.`` \emph{American Political Science Review}.
 #'
 "iver"
+
+#' Prestige of Canadian Occupations
+#'
+#' The Prestige data frame has 102 rows and 6 columns. The observations are occupations.
+#'
+#' @format
+#' This data frame contains the following columns:
+#' education: Average education of occupational incumbents, years, in 1971.
+#' income: Average income of incumbents, dollars, in 1971.
+#' women: Percentage of incumbents who are women.
+#' prestige: Pineo-Porter prestige score for occupation, from a social survey conducted in the mid-1960s.
+#' census: Canadian Census occupational code
+#' type: Type of occupation. A factor with levels (note: out of order): bc, Blue Collar; prof, Professional, Managerial, and Technical; wc, White Collar.
+#'
+#'
+#' @source car package. Canada (1971) Census of Canada. Vol. 3, Part 6. Statistics Canada [pp. 19-1–19-21].
+#' @references
+#'  Fox, J. (2008).
+#'   \emph{Applied Regression Analysis and Generalized Linear Models}, Second Edition. Sage
+#'  Fox, J. and Weisberg, S. (2011).
+#'   \emph{An R Companion to Applied Regression}, Second Edition. Sage
+"Prestige"
