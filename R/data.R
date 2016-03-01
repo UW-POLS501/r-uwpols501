fcb7d62cf5d34f1670c529570d03662d73064ae7
CasAndreu <acasas2@uw.edu>
2016-03-01 11:55:39 -0800

    adding Prestige dataset

--- R/data.R
+++ R/data.R
@@ -37,19 +37,19 @@
 
 #' Prestige of Canadian Occupations
 #'
-#' The Prestige data frame has 102 rows and 6 columns. The observations are occupations.
-#'
 #' @format
-#' This data frame contains the following columns:
-#' education: Average education of occupational incumbents, years, in 1971.
-#' income: Average income of incumbents, dollars, in 1971.
-#' women: Percentage of incumbents who are women.
-#' prestige: Pineo-Porter prestige score for occupation, from a social survey conducted in the mid-1960s.
-#' census: Canadian Census occupational code
-#' type: Type of occupation. A factor with levels (note: out of order): bc, Blue Collar; prof, Professional, Managerial, and Technical; wc, White Collar.
+#' The Prestige data frame has 102 rows and 6 columns. The observations are occupations.
+#' 
+#' \describe{
+#'  \item{education}{Average education of occupational incumbents, years, in 1971.}
+#'  \item{income}{Average income of incumbents, dollars, in 1971.}
+#'  \item{women}{Percentage of incumbents who are women.}
+#'  \item{prestige}{Pineo-Porter prestige score for occupation, from a social survey conducted in the mid-1960s.}
+#'  \item{census}{Canadian Census occupational code}
+#'  \item{type}{Type of occupation. A factor with levels (note: out of order): bc, Blue Collar; prof, Professional, Managerial, and Technical; wc, White Collar.}
+#' }
 #'
-#'
-#' @source car package. Canada (1971) Census of Canada. Vol. 3, Part 6. Statistics Canada [pp. 19-1–19-21].
+#' @source car package.
 #' @references
 #'  Fox, J. (2008).
 #'   \emph{Applied Regression Analysis and Generalized Linear Models}, Second Edition. Sage
